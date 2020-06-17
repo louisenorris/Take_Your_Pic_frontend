0487ba63e9861effc01244612d78ac1d3c2449b4
Louise Norris <louisenorris73@yahoo.co.uk>
2020-06-17 13:55:03 +0100

    styled photo gallery with css

--- src/PhotoGalleryContainer.js
+++ src/PhotoGalleryContainer.js
@@ -7,17 +7,19 @@ class PhotoGalleryContainer extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<div>
-					<h1>Polaroid gallery</h1>
+			<div className="gallery_div">
+				<>
+				<h1>Polaroid gallery</h1>
+				<div className="gallery_container">
 					{ this.props.polaroids ?
                     this.props.polaroids.map(polaroid => <PhotoCard 
                                                     key={polaroid.id} 
                                                     polaroid={polaroid} 
                                                 />)
                     : null
-                }
+                	}
 				</div>
+				</>
 			</div>
 		);
 	}
