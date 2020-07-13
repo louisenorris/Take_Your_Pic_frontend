a5affe79688bf95470027c30b7d928ded8c5bf87
Louise Norris <louisenorris73@yahoo.co.uk>
2020-07-13 17:34:02 +0100

    fixed responsive css slide down annimation of photo frame

--- src/App.js
+++ src/App.js
@@ -10,7 +10,9 @@ class App extends React.Component {
 
   state = {
     user: undefined,
-    polaroids: []
+    polaroids: [],
+    showGallery: true,
+    showCamera: false
   }
 
   componentDidMount() {
@@ -69,6 +71,16 @@ handlePhotoSave = (photo) => {
 //   .then(this.logOut())
 // }
 
+// renderContent = () => {
+//   if (this.state.user && !this.state.user.error && this.state.showGallery) {
+//     return <PhotoGalleryContainer user={this.state.user} polaroids={this.state.polaroids}/>
+//   } else if (this.state.user && !this.state.user.error && this.state.showCamera) {
+//     return <Camera user={this.state.user} handlePhotoSave={this.handlePhotoSave}/>
+//   } else {
+
+//   }
+// }
+
 
 render() {
   return (
@@ -76,6 +88,7 @@ render() {
       <div className="navbar_container">
         <Navbar user={this.state.user} signUp={this.signUp} logIn={this.logIn} logOut={this.logOut} />
       </div>
+        {/* {this.renderContent()} */}
         {
           this.state.user && !this.state.user.error && 
             <Switch>
