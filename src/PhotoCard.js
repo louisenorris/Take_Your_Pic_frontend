ef0f94f8b8d98224c9a5ab867fd39f78110029ed
Louise Norris <louisenorris73@yahoo.co.uk>
2020-08-26 09:55:57 +0100

    added date to polaroids not created by user

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -9,7 +9,7 @@ class PhotoCard extends Component {
             <div className="gallery__photo-frame">
                 <img className="gallery__photo" src={this.props.polaroid.photo.url} alt="polaroid"></img>
                 {
-                    this.props.user.id === this.props.polaroid.user_id ? <button className="photo__delete_btn" onClick={() => this.props.handleDeletePolaroid(this.props.polaroid.id)}>delete</button> : null
+                    this.props.user.id === this.props.polaroid.user_id ? <button className="photo__delete_btn" onClick={() => this.props.handleDeletePolaroid(this.props.polaroid.id)}>delete</button> : <h4 className="photo__date">{this.props.polaroid.created_at}</h4>
                 }
             </div>
         );
