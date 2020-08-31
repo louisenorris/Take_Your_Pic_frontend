4a0a53d8f406b7ef217809db6668c315d09869fa
Louise Norris <louisenorris73@yahoo.co.uk>
2020-08-31 21:27:49 +0100

    fixed date and delete button switch on hover for user polaroids

--- src/PhotoCard.js
+++ src/PhotoCard.js
@@ -3,15 +3,26 @@ import React, { Component } from 'react';
 
 class PhotoCard extends Component {
 
+    renderContent() {
+        if (this.props.user.id === this.props.polaroid.user_id ) {
+            return (
+                <>
+                <button className="photo__delete_btn" onClick={() => this.props.handleDeletePolaroid(this.props.polaroid.id)}>delete</button>
+                <h4 className="photo__date_user_polaroid">{(this.props.polaroid.created_at).slice(0, 10)}</h4>
+                </>    
+            )
+        } else {
+            return (
+                <h4 className="photo__date">{(this.props.polaroid.created_at).slice(0, 10)}</h4>
+            )
+        }
+    }
 
     render() {
         return (
             <div className="gallery__photo-frame">
                 <img className="gallery__photo" src={this.props.polaroid.photo.url} alt="polaroid"></img>
-                {
-                    this.props.user.id === this.props.polaroid.user_id ? <button className="photo__delete_btn" onClick={() => this.props.handleDeletePolaroid(this.props.polaroid.id)}>delete</button> : null
-                }
-                <h4 className="photo__date">{(this.props.polaroid.created_at).slice(0, 10)}</h4>
+                {this.renderContent()}
             </div>
         );
     }
